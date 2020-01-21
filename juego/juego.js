0b870ed9f87e1d963caa51e70b72952d2bf117dc
Jorge Eliecer <jorge.m.h@outlook.com>
2020-01-21 12:29:03 -0500

    iluminando las secuencias de colores

--- juego/juego.js
+++ juego/juego.js
@@ -14,6 +14,7 @@ class Juego {
   constructor() {
     this.inicializar()
     this.generarSecuencia()
+    this.nextLevel()
   }
   // Ocultar el botón de empezar a jugar!
   inicializar(){
@@ -28,6 +29,41 @@ class Juego {
   // Juego {secuencia: Array(10)}
   generarSecuencia() {
     this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+  }
+  // Siguiente nivel
+  nextLevel() {
+    this.iluminarSecuencia()
+  }
+
+  transformarNumeroColor(numero) {
+    switch(numero) {
+      case 0:
+        return 'celeste'
+      case 1:
+        return 'violeta'
+      case 2:
+        return 'naranja'
+      case 3:
+        return 'verde'
+    }
+  }
+  /**
+   * recorrer el array de la secuencia hasta el nivel que esté el usuario. level 1 default
+   */
+  iluminarSecuencia() {
+    for(let i = 0; i < this.level; i++) {
+      const color = this.transformarNumeroColor(this.secuencia[i])
+      setTimeout(() => this.iluminarColor(color), 1000 * i)
+    }
+  }
+
+  iluminarColor(color) {
+    this.colors[color].classList.add('light')
+    setTimeout(() => this.apagarColor(color), 350)
+  }
+
+  apagarColor(color) {
+    this.colors[color].classList.remove('light')
   }
 }
 

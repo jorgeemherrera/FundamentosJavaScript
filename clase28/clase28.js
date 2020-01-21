44920a51c840e73715158a7b7bb28d2a2615e744
Jorge Eliecer <jorge.m.h@outlook.com>
2020-01-20 22:29:35 -0500

    orden del llamado al API por callbacks()

--- clase28/clase28.js
+++ clase28/clase28.js
@@ -4,15 +4,25 @@ const PEOPLE_URL = 'people/:id'
 // const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`
 const opts = { crossDomain: true }
 
-const onPeopleResponse = function (person) {
-  console.log(`Hola yo soy, ${person.name}`)
-}
-
-function obtenerPersonaje(id) {
+function obtenerPersonaje(id, callback) {
   const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-  $.get(url, opts, onPeopleResponse)
+
+  $.get(url, opts, function (person) {
+    console.log(`Hola yo soy, ${person.name}`)
+    if(callback) {
+      callback()
+    }
+  })
 }
-obtenerPersonaje(1)
-obtenerPersonaje(2)
-obtenerPersonaje(3)
+obtenerPersonaje(1, function () {
+  obtenerPersonaje(2, function (){
+    obtenerPersonaje(3, function (){
+      obtenerPersonaje(4, function (){
+        obtenerPersonaje(5)
+      })
+    })
+  })
+})
+
+
 

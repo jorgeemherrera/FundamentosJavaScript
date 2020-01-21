b7058c933771783446082b041573a0a750bf7e80
Jorge Eliecer <jorge.m.h@outlook.com>
2020-01-20 20:49:56 -0500

    POO con JavaScript

---
+++ clase21/clase21.js
@@ -0,0 +1,17 @@
+function Persona(nombre, apellido, altura) {
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.altura = altura;
+}
+
+let jorge = new Persona('Jorge Eliécer', 'Muñoz Herrera', 170)
+let samuel = new Persona('Samuel', ' Muñoz', 185)
+
+
+Persona.prototype.soyAlto = function () {
+  if(this.altura >= 180) {
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy alto por que mido ${this.altura} cms.`);
+  } else {
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy bajo por que mido ${this.altura} cms.`);
+  }
+}
